73fca60cdc331957e664e543d2bd98d078f06fcb
Santiagoroseroq <sroseroq@unal.edu.co>
2025-01-09 19:00:45 -0500

    Create Materia.java

---
+++ src/gestorAplicacion/administracion/Materia.java
@@ -0,0 +1,11 @@
+package gestorAplicacion.administracion;
+
+public class Materia {
+    private static final long serialVersionUID=1L;
+    private String nombre;
+    private final int codigo;
+    private String descripcion;
+    private int creditos;
+    private String facultad;
+    private int cupos;
+}
