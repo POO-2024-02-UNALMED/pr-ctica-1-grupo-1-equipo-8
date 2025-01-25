80b443772088891f425623da5abc7809b7f5f144
Eslaro <semoralesm@unal.edu.co>
2025-01-24 19:01:23 -0500

    Clase coordinador completa

--- src/gestorAplicacion/administracion/Grupo.java
+++ src/gestorAplicacion/administracion/Grupo.java
@@ -1,78 +1,79 @@
 // Sergio
 package gestorAplicacion.administracion;
+
 import java.io.Serializable;
 import java.util.*;
 import gestorAplicacion.usuario.*;
 
-public class Grupo implements Serializable{
-	// Atributos de la clase
-	private int cupos;
+public class Grupo implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private Materia materia;
+	private int numero;
 	private Profesor profesor;
-	private int numero; // Del grupo 
+	private ArrayList<String> horario;
+	private int cupos;
 	private Salon salon;
-	private Arraylist<String> horario;
-	private Arraylist<Estudiante> estudiantes;
-	
-	// ***Constructores***
+	private ArrayList<Estudiante> estudiantes;
+	private static ArrayList<Grupo> gruposTotales = new ArrayList<Grupo>();
 
-	// Primer constructor	
-	public Grupo (Materia materia, int numero, Profesor profesor, Arraylist<String> horario, int cupos, Salon salon ){
+	public Grupo(Materia materia, int numero, Profesor profesor, ArrayList<String> horario, int cupos, Salon salon) {
 		this.materia = materia;
 		this.numero = numero;
 		this.profesor = profesor;
 		this.horario = horario;
 		this.cupos = cupos;
 		this.salon = salon;
-		this.estudiantes = new Arraylist<Estudiante>();
-	}
-	// Segundo constructor
-	public Grupo(Materia materia, int numero, Profesor profesor){
-		this.materia = materia;
-		this.numero=numero;
-		this.profesor=profesor;
+		this.estudiantes = new ArrayList<Estudiante>();
 		Grupo.gruposTotales.add(this);
-	  } 	
-	// Metodos
+	}
 
-	public String mostrarInformacionGrupo(){
-		String retorno = "Numero del grupo: "+this.numero+", Profesor: "+this.profesor+ ", Horario: "+this.horario+", Cupos: "+this.cupos+"Salon: "+this.salon;
+	public Grupo(Materia materia, int numero, Profesor profesor) {
+		this.materia = materia;
+		this.numero = numero;
+		this.profesor = profesor;
+		Grupo.gruposTotales.add(this);
+	}
+
+	public String mostrarInformacionGrupo() {
+		String retorno = "Número del grupo: " + this.numero + ", Profesor: " + this.profesor + ", Horario: "
+				+ this.horario + ", Cupos: " + this.cupos + ", Salón: " + this.salon;
 		return retorno;
 	}
-	
 
-	public boolean existenciaEstudiante(Estudiante estudiante){
-		for (int j=0;j<this.estudiantes.size();j++){
-		  if (this.estudiantes.get(j).getId() == estudiante.getId()){
-			return true;
-		  }
-		}
-		return false;
-	  }
-	public void eliminarEstudiante(Estudiante estudiante){
-		int indice = -1;
-		for (int i=0;i<this.estudiantes.size();i++){
-			if (this.estudiantes.get(i).getNombre().equals(estudiante.getNombre())){
-					indice = i;
-					this.cupos++;
-					estudiante.eliminarGrupo(this);
-					break;
+	public boolean existenciaEstudiante(Estudiante estudiante) {
+		for (int j = 0; j < this.estudiantes.size(); j++) {
+			if (this.estudiantes.get(j).getId() == estudiante.getId()) {
+				return true;
 			}
 		}
-		if (indice != -1){
+		return false;
+	}
+
+	public void eliminarEstudiante(Estudiante estudiante) {
+		int indice = -1;
+		for (int i = 0; i < this.estudiantes.size(); i++) {
+			if (this.estudiantes.get(i).getNombre().equals(estudiante.getNombre())) {
+				indice = i;
+				this.cupos++;
+				estudiante.eliminarGrupo(this);
+				break;
+			}
+		}
+		if (indice != -1) {
 			this.estudiantes.remove(indice);
 		}
-    }
-	public static Grupo buscarGrupo(Materia materia, Grupo grupo){
-		int indicei = 0;
-		int indicej = 0;
-		for (int i = 0; i<Materia.getMateriasTotales().size(); i++){
+	}
+
+	public static Grupo buscarGrupo(Materia materiaE, Grupo grupoE) {
+		int indicei = -1;
+		int indicej = -1;
+		for (int i = 0; i < Materia.getMateriasTotales().size(); i++) {
 			Materia materia = Materia.getMateriasTotales().get(i);
-			if (materia.getNombre().equals(materiaE.getNombre())){
+			if (materia.getNombre().equals(materiaE.getNombre())) {
 				indicei = i;
-				for (int j = 0; j<materia.getGrupos().size(); j++){
+				for (int j = 0; j < materia.getGrupos().size(); j++) {
 					Grupo grupo = materia.getGrupos().get(j);
-					if (grupo.getNumero() == grupoE.getNumero()){
+					if (grupo.getNumero() == grupoE.getNumero()) {
 						indicej = j;
 						break;
 					}
@@ -81,10 +82,12 @@ public class Grupo implements Serializable{
 		}
 		return Materia.getMateriasTotales().get(indicei).getGrupos().get(indicej);
 	}
-	public void agregarEstudiante(Estudiante estudiante){
+
+	public void agregarEstudiante(Estudiante estudiante) {
 		this.estudiantes.add(estudiante);
-		cupos--;
+		this.cupos--;
 	}
+
 	public int getNumero() {
 		return this.numero;
 	}
@@ -132,11 +135,11 @@ public class Grupo implements Serializable{
 	public void setEstudiantes(ArrayList<Estudiante> estudiantes) {
 		this.estudiantes = estudiantes;
 	}
-	
+
 	public Materia getMateria() {
 		return this.materia;
 	}
-	
+
 	public void setMateria(Materia materia) {
 		this.materia = materia;
 	}
@@ -144,4 +147,5 @@ public class Grupo implements Serializable{
 	public static ArrayList<Grupo> getGruposTotales() {
 		return gruposTotales;
 	}
+
 }
--- src/gestorAplicacion/usuario/Coordinador.java
+++ src/gestorAplicacion/usuario/Coordinador.java
@@ -4,100 +4,104 @@ package gestorAplicacion.usuario;
 import java.util.ArrayList;
 import gestorAplicacion.administracion.*;
 import java.io.Serializable;
-public class Coordinador extends Usuario implements Serializable{
-    private final static int limitesCreditos=20;
+
+public class Coordinador extends Usuario implements Serializable {
+    private final static int limitesCreditos = 20;
     private static final long serialVersionUID = 1L;
     private static ArrayList<Coordinador> coordinadoresTotales = new ArrayList<Coordinador>();
-    private static String[] facultades = {"Facultad de arquitectura", "Facultad de ciencias", "Facultad de ciencias agrarias" , "Facultad de ciencias humanas y economicas", "Facultad de minas", "Sede"};
+    private static String[] facultades = { "Facultad de arquitectura", "Facultad de ciencias",
+            "Facultad de ciencias agrarias", "Facultad de ciencias humanas y economicas", "Facultad de minas", "Sede" };
 
-    public Coordinador(String facultad,long id, String nombre, String pw) {
-        super(id, nombre,pw, facultad);
+    public Coordinador(String facultad, long id, String nombre, String pw) {
+        super(id, nombre, pw, facultad);
         super.setTipo("Coordinador");
         Coordinador.coordinadoresTotales.add(this);
     }
 
     // Desmatricula a el estudiante de un grupo
-    public String desmatricular(Estudiante estudiante, Grupo grupo){
+    public String desmatricular(Estudiante estudiante, Grupo grupo) {
 
         boolean estaMatriculado = grupo.existenciaEstudiante(estudiante);
 
-        if (estaMatriculado){
+        if (estaMatriculado) {
             grupo.eliminarEstudiante(estudiante);
             return "El estudiante ha sido desmatriculado de la materia y su respectivo grupo";
-        }
-        else{
+        } else {
             return "El estudiante no estaba matriculado";
         }
     }
-    public void resturarMateria(Materia materia){
-        for (int i=0;i<materia.getGrupos().size();i++){
+
+    public void resturarMateria(Materia materia) {
+        for (int i = 0; i < materia.getGrupos().size(); i++) {
             Grupo puntero_Grupo = materia.getGrupos().get(i);
             puntero_Grupo.getProfesor().desvincularGrupo(puntero_Grupo);
 
-            for (int j=0;j<puntero_Grupo.getEstudiantes().size();j++){
+            for (int j = 0; j < puntero_Grupo.getEstudiantes().size(); j++) {
                 Estudiante puntero_Estudiante = puntero_Grupo.getEstudiantes().get(j);
                 this.desmatricular(puntero_Estudiante, puntero_Grupo);
                 // Dentro de este metodo se llama al metodo desmatricular
-            }   
+            }
         }
     }
-    public void desmatricularDelSistema(Usuario estudiante){
+
+    public void desmatricularDelSistema(Usuario estudiante) {
         Estudiante e1 = null;
-        for (Estudiante e: Estudiante.getEstudiantes()){
-            if (e ==(Estudiante)estudiante){
+        for (Estudiante e : Estudiante.getEstudiantes()) {
+            if (e == (Estudiante) estudiante) {
                 e1 = e;
             }
         }
-        if (e1 != null){
+        if (e1 != null) {
             Estudiante.getEstudiantes().remove(e1);
         }
-        for (Usuario usuario: Usuario.getUsuariosTotales()){
-            if (usuario instanceof Estudiante){
-                if (((Estudiante) usuario).equals(estudiante)){
+        for (Usuario usuario : Usuario.getUsuariosTotales()) {
+            if (usuario instanceof Estudiante) {
+                if (((Estudiante) usuario).equals(estudiante)) {
                     Usuario.getUsuariosTotales().remove(usuario);
                     break;
                 }
             }
         }
     }
-    public static Object[] crearHorario(ArrayList<Materia> materias){
+
+    public static Object[] crearHorario(ArrayList<Materia> materias) {
         Object[] resultado = new Object[3];
-                
-        Horario horario =  new Horario();
+
+        Horario horario = new Horario();
         boolean ok = true;
         Materia materiaObstaculo = null;
 
-        int[] gPosible = new int[materias.size()];   
-        int[] mPosibles = new int[materias.size()];    
-        int i =0; // indice de materias
-        
-        while (true){
+        int[] gPosible = new int[materias.size()];
+        int[] mPosibles = new int[materias.size()];
+        int i = 0; // indice de materias
+
+        while (true) {
             ArrayList<String> pClases = materias.get(i).getGrupos().get(gPosible[i]).getHorario();
-            if (horario.comprobarDisponibilidad(pClases)){
-                
+            if (horario.comprobarDisponibilidad(pClases)) {
+
                 horario.ocuparHorario(materias.get(i).getGrupos().get(gPosible[i]));
-                mPosibles[i] =1;
+                mPosibles[i] = 1;
                 i++;
-                if (i==materias.size()){
+                if (i == materias.size()) {
                     break;
                 }
 
-            }else{
+            } else {
                 gPosible[i]++;
 
-                if (gPosible[i]==materias.get(i).getGrupos().size()){
+                if (gPosible[i] == materias.get(i).getGrupos().size()) {
                     i--;
                     horario.liberarHorario(materias.get(i).getGrupos().get(gPosible[i]).getHorario());
                     gPosible[i]++;
-                    gPosible[i+1]=0;
+                    gPosible[i + 1] = 0;
 
-                    if (gPosible[i]==materias.get(i).getGrupos().size()){
-                        int m =0;
-                        for (int k:mPosibles){
-                            if (k==0){
+                    if (gPosible[i] == materias.get(i).getGrupos().size()) {
+                        int m = 0;
+                        for (int k : mPosibles) {
+                            if (k == 0) {
                                 materiaObstaculo = materias.get(m);
                                 ok = false;
-                            }else{
+                            } else {
                                 m++;
                             }
                         }
@@ -113,4 +117,77 @@ public class Coordinador extends Usuario implements Serializable{
 
         return resultado;
     }
+
+    public void eliminarMateria(Materia materia) {
+        if (Materia.getMateriasTotales().contains(materia)) {
+            Materia.getMateriasTotales().remove(materia);
+            resturarMateria(materia);
+        }
+
+    }
+
+    public void agregarMateria(String nombre, int codigo, String descripcion, int creditos, String facultad,
+            ArrayList<Materia> prerrequisitos) {
+        ArrayList<String> nombreMaterias = new ArrayList<String>();
+        for (Materia materia : Materia.getMateriasTotales()) {
+            nombreMaterias.add(materia.getNombre());
+        }
+        if (nombreMaterias.contains(nombre) == false) {
+            Materia nMateria = new Materia(nombre, codigo, descripcion, creditos, facultad, prerrequisitos);
+        }
+    }
+
+    public boolean candidatoABeca(Estudiante estudiante, Beca tipoDeBeca) {
+        if (tipoDeBeca.getCupos() > 0) {
+            if ((estudiante.getPromedio() >= tipoDeBeca.getPromedioRequerido())
+                    && (estudiante.getAvance() >= tipoDeBeca.getAvanceRequerido()) &&
+                    (estudiante.getCreditos() >= tipoDeBeca.getCreditosInscritosRequeridos())
+                    && (estudiante.getEstrato() <= tipoDeBeca.getEstratoMinimo())) {
+                if (tipoDeBeca.getNecesitaRecomendacion()) {
+                    if (Profesor.recomendarEstudiante(estudiante)) {
+                        return true;
+                    } else
+                        return false;
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static String mostrarFacultades() {
+        String retorno = "";
+        int i = 1;
+        for (String facultad : facultades) {
+            retorno += (i++) + ". " + facultad + "\n";
+        }
+        return retorno;
+    }
+
+    public String toString() {
+        return "Nombre: " + getNombre() + "\nDocumento: " + getId();
+    }
+
+    // Getters y Setters
+
+    public static int getLimitesCreditos() {
+        return limitesCreditos;
+    }
+
+    public static ArrayList<Coordinador> getCoordinadoresTotales() {
+        return coordinadoresTotales;
+    }
+
+    public static String[] getFacultades() {
+        return facultades;
+    }
+
+    public static void setFacultades(String[] facultades) {
+        Coordinador.facultades = facultades;
+    }
+
 }
