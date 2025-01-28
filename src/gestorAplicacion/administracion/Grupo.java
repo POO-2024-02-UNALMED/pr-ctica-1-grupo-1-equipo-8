2ace611cbb399143b8e91eecfcf56ffa038150fc
Eslaro <semoralesm@unal.edu.co>
2025-01-27 21:37:11 -0500

    Revert "Codigo hermoso y comentado"
    This reverts commit 524afbfcb49b9acfdce94f73b2b64e45affe5c52.

--- src/gestorAplicacion/administracion/Grupo.java
+++ src/gestorAplicacion/administracion/Grupo.java
@@ -16,7 +16,6 @@ public class Grupo implements Serializable {
 	private ArrayList<Estudiante> estudiantes;
 	private static ArrayList<Grupo> gruposTotales = new ArrayList<Grupo>();
 
-	// Constructores de la clase Grupo
 	public Grupo(Materia materia, int numero, Profesor profesor, ArrayList<String> horario, int cupos, Salon salon) {
 		this.materia = materia;
 		this.numero = numero;
@@ -35,16 +34,12 @@ public class Grupo implements Serializable {
 		Grupo.gruposTotales.add(this);
 	}
 
-	// Metodos
-
-	// metodo que muestra la informacion de un grupo
 	public String mostrarInformacionGrupo() {
 		String retorno = "Número del grupo: " + this.numero + ", Profesor: " + this.profesor + ", Horario: "
 				+ this.horario + ", Cupos: " + this.cupos + ", Salón: " + this.salon;
 		return retorno;
 	}
 
-	// metodo que verifica si un estudiante esta matriculado en un grupo
 	public boolean existenciaEstudiante(Estudiante estudiante) {
 		for (int j = 0; j < this.estudiantes.size(); j++) {
 			if (this.estudiantes.get(j).getId() == estudiante.getId()) {
@@ -54,7 +49,6 @@ public class Grupo implements Serializable {
 		return false;
 	}
 
-	// metodo que elimina un estudiante de un grupo
 	public void eliminarEstudiante(Estudiante estudiante) {
 		int indice = -1;
 		for (int i = 0; i < this.estudiantes.size(); i++) {
@@ -63,7 +57,6 @@ public class Grupo implements Serializable {
 				this.cupos++;
 				estudiante.eliminarGrupo(this);
 				break;
-				// Se elimina al estudiante del grupo y se le elimina el grupo al estudiante
 			}
 		}
 		if (indice != -1) {
@@ -71,7 +64,6 @@ public class Grupo implements Serializable {
 		}
 	}
 
-	// metodo que busca un grupo
 	public static Grupo buscarGrupo(Materia materiaE, Grupo grupoE) {
 		int indicei = -1;
 		int indicej = -1;
@@ -89,16 +81,13 @@ public class Grupo implements Serializable {
 			}
 		}
 		return Materia.getMateriasTotales().get(indicei).getGrupos().get(indicej);
-		// Regresa el grupo de acuerdo a los indices encontrados
 	}
 
-	// metodo que agrega un estudiante a un grupo
 	public void agregarEstudiante(Estudiante estudiante) {
 		this.estudiantes.add(estudiante);
 		this.cupos--;
 	}
 
-	// Getters y Setters
 	public int getNumero() {
 		return this.numero;
 	}
